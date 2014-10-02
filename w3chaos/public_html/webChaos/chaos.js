4299f7da559496c5172e4123c15abd04f0acd1e2
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2014-10-02 18:48:31 +0200

    power supply, error decoding

--- w3chaos/public_html/webChaos/chaos.js
+++ w3chaos/public_html/webChaos/chaos.js
@@ -138,7 +138,10 @@ function CU(name){
            request.open("GET", "/cgi-bin/cu.cgi?status=" + this.name, true);
 
         }
-        request.ontimeout = function () { alert("Timed out!!!"); }
+        request.ontimeout = function () { 
+            //alert("Timed out!!!"); 
+            console.log("TIMEOUT!!");
+        }
 
         request.send();
         request.onreadystatechange = function() {
@@ -196,7 +199,43 @@ function CU(name){
 } 
 }
 
-
+ function addRow(tableID) {
+ 
+            var table = document.getElementById(tableID);
+ 
+            var rowCount = table.rows.length;
+            var row = table.insertRow(rowCount);
+            return row;
+            
+ /*
+            var cell1 = row.insertCell(0);
+            var element1 = document.createElement("input");
+            element1.type = "checkbox";
+            element1.name="chkbox[]";
+            cell1.appendChild(element1);
+ 
+            var cell2 = row.insertCell(1);
+            cell2.innerHTML = rowCount + 1;
+ 
+            var cell3 = row.insertCell(2);
+            var element2 = document.createElement("input");
+            element2.type = "text";
+            element2.name = "txtbox[]";
+            cell3.appendChild(element2);
+ 
+ */
+        }
+ 
+        function deleteRow(tableID) {
+            try {
+            var table = document.getElementById(tableID);
+            var rowCount = table.rows.length;
+            table.deleteRow(rowCount);
+        
+            }catch(e) {
+            }
+        }
+        
 function CULoad(classname){
      var query = window.location.search.substring(1);
      var vars = query.split("=");
--- w3chaos/public_html/webChaos/powersupply.js
+++ w3chaos/public_html/webChaos/powersupply.js
@@ -50,13 +50,45 @@ function PowerSupply(name){
     }
 
 
- 
+ function decodeError(alarm, table){
+     table.innerHTML = "";
+     var rows=0;
+       for(var a=0;a<32;a++){
+           var bit = alarm & (1<<a);
+           var row,cell;
+           if(bit){
+               row = table.insertRow(rows++);
+               cell = row.insertCell(0);
+           }
+           if(bit&0x1){
+               cell.innerHTML = "1 fusibili area";
+           } else if(bit&0x2){
+               cell.innerHTML = "2 sovracorrente AC ";
+           } else if(bit&0x4){
+               cell.innerHTML ="3 fusibili IGBT";
+           } else if(bit&0x8){
+               cell.innerHTML ="4 sovra temperatura IGBT";
+           } else if(bit&0x10){
+               cell.innerHTML ="5 sovra corrente DCLINK";
+           } else if(bit&0x20){
+               cell.innerHTML = "6 AirFlow ";
+           } else if(bit&0x40){
+               cell.innerHTML ="7 ---- ";
+               
+           } else if(bit&0x80){
+               cell.innerHTML = "8 Sovra temperatura raddrizzatore ";
+           } else if(bit>0){
+               cell.innerHTML = a + " --- ";
+
+           }
+       } 
+ }
  function powerSupplyUpdateArrayInterface(){
                 CUupdateInterface();
                 for(var i = 0;i<cus.length;i++){
                     cus[i].update();
                     console.log("updating \""+cus[i].name + " current:"+cus[i].current + " polarity:"+cus[i].polarity);
-                    
+                    var table_error = document.getElementById("error_list_"+i);
                     if(cus[i].state&0x2){
                         document.getElementById("onstby_"+i).value="On";
                         document.getElementById("neg_"+i).disabled=true;
@@ -83,8 +115,14 @@ function PowerSupply(name){
                     }
                     
                     if(cus[i].alarms!=0){
-                        document.getElementById("alarms_"+i).style.color="red";
-                    } 
+                        var alarm=cus[i].alarms
+                       
+                        decodeError(alarm,table_error);
+                       
+                        
+                    } else {
+                        table_error.innerHTML="";
+                    }
                     $.plot("#powersupply-graph_"+i,[cus[i].points]);
                 }
             }
