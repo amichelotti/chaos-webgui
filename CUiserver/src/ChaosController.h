30013b55ada907ca129b73ff1afa704843c30409
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2015-06-18 10:37:59 +0200

    fix

--- CUiserver/src/ChaosController.h
+++ CUiserver/src/ChaosController.h
@@ -22,7 +22,7 @@ using namespace chaos;
 #define MDS_TIMEOUT 3000
 #define MDS_STEP_TIMEOUT 1000
 #define MDS_RETRY 3
-#define MAX_STRING 1024
+
 struct InfoDevice {
   chaos::ui::DeviceController* dev;
   std::string devname;
@@ -31,6 +31,15 @@ struct InfoDevice {
   int lastState;
   int defaultTimeout;
   int wostate;// without state
+  int nextState;
+  uint64_t lastReq;
+  uint64_t oldReq;
+  uint64_t firstReq;
+  uint64_t timestamp;
+  
+  InfoDevice(){lastReq=0;nextState=-1;timestamp=0;lastState=-1;firstReq=0;}
+ ~InfoDevice(){}
+ 
 };
 
 class ChaosController : public WebController
@@ -38,11 +47,10 @@ class ChaosController : public WebController
 private:
     static std::map<std::string,InfoDevice*> devs; 
     int mds_timeout;
-    int sendCmd(chaos::ui::DeviceController *controller ,std::string cmd_alias_str,char*param);
+    int sendCmd(chaos::ui::DeviceController *controller ,std::string& cmd_alias_str,char*param,Request & request,dev_info_status&status);
     int sendAttr(DeviceController *controller ,std::string cmd_alias_str,char*param);
     CDataWrapper* fetchDataSet(DeviceController *ctrl);
-    char *jsondest;
-    int size_json;
+   
     public: 
         ChaosController();
         void handleCU(Request &request, StreamResponse &response);
--- CUiserver/src/dev_status.h
+++ CUiserver/src/dev_status.h
@@ -10,58 +10,79 @@
 
 #include "common/debug/debug.h"
 #include <chaos/common/chaos_constants.h>
- using   namespace chaos;
- #define CUIServerLAPP_		LAPP_ << "[CUIServer] "
+#define MAX_STRING 1024
+using namespace chaos;
+#define CUIServerLAPP_		LAPP_ << "[CUIServer] "
 #define CUIServerLDBG_		LDBG_ << "[CUIServer] "
 #define CUIServerLERR_		LERR_ << "[CUIServer] "
 
-struct dev_info_status{
-  char dev_status[256];
-  char error_status[256];
-  char log_status[256];
-  dev_info_status(){*dev_status=0;*error_status=0;*log_status=0;}
+struct dev_info_status {
+    char dev_status[256];
+    char error_status[256];
+    char log_status[256];
+    std::auto_ptr<chaos::common::data::CDataWrapper> data_wrapper;
+    dev_info_status() {
+        *dev_status = 0;
+        *error_status = 0;
+        *log_status = 0;
+        data_wrapper.reset(new chaos::common::data::CDataWrapper());
+    }
 
-  void status(CUStateKey::ControlUnitState deviceState){
-    if(deviceState==CUStateKey::DEINIT){
-      strcpy(dev_status,"deinit");
-    } else if(deviceState == CUStateKey::INIT){
-        strcpy(dev_status,"init");
-    } else if(deviceState == CUStateKey::START){
-        strcpy(dev_status,"start");
-    } else if(deviceState == CUStateKey::STOP){
-          strcpy(dev_status,"stop");
-    } else {
-        strcpy(dev_status,"uknown");
+    void status(CUStateKey::ControlUnitState deviceState) {
+        if (deviceState == CUStateKey::DEINIT) {
+            strcpy(dev_status, "deinit");
+        } else if (deviceState == CUStateKey::INIT) {
+            strcpy(dev_status, "init");
+        } else if (deviceState == CUStateKey::START) {
+            strcpy(dev_status, "start");
+        } else if (deviceState == CUStateKey::STOP) {
+            strcpy(dev_status, "stop");
+        } else {
+            strcpy(dev_status, "uknown");
+        }
     }
-  }
-  void append_log(std::string log){
-      CUIServerLAPP_<<log;
-    snprintf(log_status,sizeof(log_status),"%s%s;",log_status,log.c_str());
- 
-  }
-  void append_error(std::string log){
-    CUIServerLERR_<<log;
-    snprintf(error_status,sizeof(error_status),"%s%s;",error_status,log.c_str());
- 
-  }
-  void insert_json(char*json){
+
+    void append_log(std::string log) {
+        CUIServerLAPP_ << log;
+        snprintf(log_status, sizeof (log_status), "%s%s;", log_status, log.c_str());
+
+    }
+
+    void append_error(std::string log) {
+        CUIServerLERR_ << log;
+        snprintf(error_status, sizeof (error_status), "%s%s;", error_status, log.c_str());
+
+    }
+
+    void insert_json(char*json) {
+        if (json == NULL) return;
+
+        if (*json == 0) {
+            sprintf(json, "{\"dev_status\":\"%s\",\"error_status\":\"%s\",\"log_status\":\"%s\"}", dev_status, error_status, log_status);
+            return;
+        }
+
+        char *t = strstr(json,",\"dev_status");
+        if(t==NULL){
+
+            while (*json != 0) {
+
+                if (*json == '}') {
+                    t = json;
+                }
+                json++;
+            }
+        }
+        if (t) sprintf(t, ",\"dev_status\":\"%s\",\"error_status\":\"%s\",\"log_status\":\"%s\"}", dev_status, error_status, log_status);
+    }
     
-    if(json==NULL) return;
-    if(*json==0){
-        sprintf(json,"{\"dev_status\":\"%s\",\"error_status\":\"%s\",\"log_status\":\"%s\"}",dev_status,error_status,log_status);
-	return;
+    CDataWrapper * getData(){
+         data_wrapper->addStringValue("dev_status",std::string(dev_status));
+     data_wrapper->addStringValue("log_status",std::string(log_status));
+     data_wrapper->addStringValue("error_status",std::string(error_status));
+
+        return data_wrapper.get();
     }
-    while(*json!=0){
-        
-      if(*json=='{'){
-          char temp[4096];
-          strncpy(temp,json+1,4096);
-	  sprintf(json,"{\"dev_status\":\"%s\",\"error_status\":\"%s\",\"log_status\":\"%s\",%s",dev_status,error_status,log_status,temp);
-	return;
-      } 
-        json++;
-    }
-  }
 };
 
 
