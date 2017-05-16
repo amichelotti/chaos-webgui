873b5a1e0b53e3cd66cca2c93185a54320d3bbdf
amichelo <andrea.michelotti@lnf.infn.it>
2017-05-16 16:57:44 +0200

    added accumulator orbit

---
+++ w3chaos/public_html/accumulatorOrbit/js/chartSA.js
@@ -0,0 +1,235 @@
+     var array_bpm_nome = ["BPBA1001X","BPBA1002X","BPBA2001X","BPBA2002X","BPBA3001X","BPBA3002X","BPBA4001X","BPBA4002X","BPSA1001X","BPSA2001X", 
+                                    "BPSA3001X","BPSA4001X"];
+                  
+                  var array_bpm_nomeY = ["BPBA1001Y","BPBA1002Y","BPBA2001Y","BPBA2002Y","BPBA3001Y","BPBA3002Y","BPBA4001Y","BPBA4002Y","BPSA1001Y","BPSA2001Y", 
+                                    "BPSA3001Y","BPSA4001Y"];
+                  
+                  var nome_bpmSum = ["BPBA1001SUM","BPBA1002SUM","BPBA2001SUM","BPBA2002SUM","BPBA3001SUM","BPBA3002SUM","BPBA4001SUM","BPBA4002SUM","BPSA1001SUM","BPSA2001SUM", 
+                                    "BPSA3001SUM","BPSA4001SUM"];
+
+function buildBoxPlotSA(title){
+    
+      var array_bpm = ["BPBA1001","BPBA1002","BPBA2001","BPBA2002","BPBA3001","BPBA3002","BPBA4001","BPBA4002","BPSA1001","BPSA2001", 
+		    "BPSA3001","BPSA4001"]; 
+    
+      if ($('#multi_plot').length){
+    	        
+            $("#multi_plot").append('<div class="row"><div class="col s12 m12 l12"><h3 class="plot_title">------  ' + title+ '  -----</h3></div></div>');
+      
+            for(i = 0; i < array_bpm.length; i ++ ){
+                  
+                   $('#multi_plot').append('<div class="row"><div class="col s3 m3 l3 name_dvc"><div class="rTableCell device"><b>'+array_bpm[i] +'</b></div>'
+				    + '</div></div>' 
+				    +'<div class="row"><div class="col s12 m12 l12">'
+				    +'<div class="col s12 m12 l4"><fieldset class="plot"><legend>SUM [V]</legend>'
+				    + '<div id="plotSum' +[i] +'" style="width:98%; height:260px;"></div></fieldset></div>'
+				    +'<div class="col s12 m12 l4"><fieldset class="plot"><legend>X [mm]</legend>'
+				    + '<div id="plotX' +[i] +'" style="width:98%; height:260px;"></div></fieldset></div>'
+				    +'<div class="col s12 m12 l4"><fieldset class="plot"><legend>Y [mm]</legend>'
+				    + '<div id="plotY' +[i] +'" style="width:98%; height:260px;"></div></fieldset></div>'
+				    +'</div></div>');
+                  
+               /* $('#multi_plot').append('<div class="row"><div class="col s12 m12 l12"><div class="col s2 m2 l2 name_dvc"><div class="rTableCell device"><b>'+array_bpm[i] +'</b></div>'
+				    + '</div><div class="col s9 m9 l5"><fieldset class="plot"><legend>X [mm]</legend>'
+					+ '<div id="plotX' +[i] +'" style="width:98%; height:260px;"></div>' 
+					+'</fieldset></div><div class="col s9 m9 l5 offset-s2 offset-m2"><fieldset class="plot">' 
+					+ '<legend>Y [mm]</legend><div id="plotY'+[i]+'" style="width:98%; height:260px;"></div>' 
+					+'</fieldset></div></div></div>'); */
+            }
+      	
+    
+      plotSA();
+       
+      } else {
+            
+            $(".box-multiple-plot").append('<div id="multi_plot"></div>');
+  
+            $("#multi_plot").append('<div class="row"><div class="col s12 m12 l12"><h3 class="plot_title">------  ' + title+ '  -----</h3></div></div>');
+      
+            for(i = 0; i < array_bpm.length; i ++ ){
+                  
+                   $('#multi_plot').append('<div class="row"><div class="col s3 m3 l3 name_dvc"><div class="rTableCell device"><b>'+array_bpm[i] +'</b></div>'
+				    + '</div></div>' 
+				    +'<div class="row"><div class="col s12 m12 l12">'
+				    +'<div class="col s12 m12 l4"><fieldset class="plot"><legend>SUM [V]</legend>'
+				    + '<div id="plotSum' +[i] +'" style="width:98%; height:260px;"></div></fieldset></div>'
+				    +'<div class="col s12 m12 l4"><fieldset class="plot"><legend>X [mm]</legend>'
+				    + '<div id="plotX' +[i] +'" style="width:98%; height:260px;"></div></fieldset></div>'
+				    +'<div class="col s12 m12 l4"><fieldset class="plot"><legend>Y [mm]</legend>'
+				    + '<div id="plotY' +[i] +'" style="width:98%; height:260px;"></div></fieldset></div>'
+				    +'</div></div>');
+                   
+                   
+               /* $('#multi_plot').append('<div class="row"><div class="col s12 m12 l12"><div class="col s2 m2 l2 name_dvc"><div class="rTableCell device"><b>'+array_bpm[i] +'</b></div>'
+				    + '</div><div class="col s9 m9 l5"><fieldset class="plot"><legend>X [mm]</legend>'
+					+ '<div id="plotX' +[i] +'" style="width:98%; height:260px;"></div>' 
+					+'</fieldset></div><div class="col s9 m9 l5 offset-s2 offset-m2"><fieldset class="plot">' 
+					+ '<legend>Y [mm]</legend><div id="plotY'+[i]+'" style="width:98%; height:260px;"></div>' 
+					+'</fieldset></div></div></div>'); */
+            }
+      	
+            plotSA();
+	
+      }
+} 
+
+
+function plotSA() {
+      
+      var updateInterval = 500;
+      var totalPoints = 100;
+
+      var globalDataX = new Array(12);
+      var globalDataY = new Array(12);
+      var globalDataSum = new Array(12);
+      for (var i = 0; i< globalDataX.length; i ++) {
+            globalDataX[i] = [];
+            globalDataY[i] = [];
+            globalDataSum[i] = [[],[]];
+      }
+    
+      var setX = new Array(12);
+      var setY = new Array(12);
+      var setSum = new Array(12);
+      for (var i = 0; i< globalDataX.length; i ++) {
+            setX[i] = [];
+            setY[i] = [];
+            setSum[i] = [];
+      }
+
+      var nowX = new Array(12);
+      var nowY = new Array(12);
+      var nowSum = new Array(12);
+      for (var i = 0; i< nowX.length; i ++) {
+            nowX[i] = new Date().getTime();
+            nowY[i] = new Date().getTime();
+            nowSum[i] = new Date().getTime();
+      }
+    
+
+      function GetData() {
+     /*       $.get("http://" + location.host + ":8081/CU?dev=ACCUMULATOR/BPM/BPMSYNC&cmd=status", function(datavalue, textStatus) {
+                  var data_json = datavalue.replace(/\$numberLong/g, 'numberLong');
+                  data_json = data_json.replace(/\//g, '');
+                  var obj_json = JSON.parse(data_json);
+                        
+                  var array_bpm_nome = ["BPBA1001X","BPBA1002X","BPBA2001X","BPBA2002X","BPBA3001X","BPBA3002X","BPBA4001X","BPBA4002X","BPSA1001X","BPSA2001X", 
+                                    "BPSA3001X","BPSA4001X"];
+                  
+                  var array_bpm_nomeY = ["BPBA1001Y","BPBA1002Y","BPBA2001Y","BPBA2002Y","BPBA3001Y","BPBA3002Y","BPBA4001Y","BPBA4002Y","BPSA1001Y","BPSA2001Y", 
+                                    "BPSA3001Y","BPSA4001Y"];
+                  
+                  var nome_bpmSum = ["BPBA1001SUM","BPBA1002SUM","BPBA2001SUM","BPBA2002SUM","BPBA3001SUM","BPBA3002SUM","BPBA4001SUM","BPBA4002SUM","BPSA1001SUM","BPSA2001SUM", 
+                                    "BPSA3001SUM","BPSA4001SUM"];
+
+                        
+                  for (var i=0; i<12; i++){
+                        globalDataX[i].shift();
+                        while(globalDataX[i].length <totalPoints){
+                              globalDataX[i].push([nowX[i] += updateInterval, obj_json[array_bpm_nome[i]]]);
+                        }          
+                  }
+                  
+                  for (var i=0; i<12; i++){
+                        globalDataY[i].shift();
+                        while(globalDataY[i].length <totalPoints){
+                              globalDataY[i].push([nowY[i] += updateInterval, obj_json[array_bpm_nomeY[i]]]);
+                        }          
+                  }
+                  
+                  for (var i=0; i<12; i++){
+                        globalDataSum[i].shift();
+                        while(globalDataSum[i].length <totalPoints){
+                              globalDataSum[i].push([nowSum[i] += updateInterval, obj_json[nome_bpmSum[i]]]);
+                        }          
+                  }
+            
+            });
+            */
+    	  for (var i=0; i<12; i++){
+              globalDataX[i].shift();
+              while(globalDataX[i].length <totalPoints){
+                    globalDataX[i].push([nowX[i] += updateInterval, obj_json[array_bpm_nome[i]]]);
+              }          
+        }
+        
+        for (var i=0; i<12; i++){
+              globalDataY[i].shift();
+              while(globalDataY[i].length <totalPoints){
+                    globalDataY[i].push([nowY[i] += updateInterval, obj_json[array_bpm_nomeY[i]]]);
+              }          
+        }
+        
+        for (var i=0; i<12; i++){
+              globalDataSum[i].shift();
+              while(globalDataSum[i].length <totalPoints){
+                    globalDataSum[i].push([nowSum[i] += updateInterval, obj_json[nome_bpmSum[i]]]);
+              }          
+        }
+      }
+              
+        
+      var options = {
+            series: {
+                  lines: {
+                      show: true,
+                      lineWidth: 1.2
+                  }
+            },
+            xaxis: {
+                  mode: "time",
+                  tickSize: [2, "second"],
+                  tickFormatter: function (v, axis) {
+                        var date = new Date(v);
+                        if (date.getSeconds() % 30 == 0) {
+                              var hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+                              var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+                              var seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+                              return hours + ":" + minutes + ":" + seconds;
+                        } else {
+                              return "";
+                        } 
+                  }, 
+                  axisLabel: "Time [h:m:s]",
+                  axisLabelUseCanvas: true,
+                  axisLabelFontSizePixels: 12,
+                  axisLabelFontFamily: 'Verdana, Arial',
+                  axisLabelPadding: 10
+            },
+            yaxis: {
+                  tickDecimals: [3],
+                  axisLabel: "[mm]",
+                  axisLabelUseCanvas: true,
+                  axisLabelFontSizePixels: 12,
+                  axisLabelFontFamily: 'Verdana, Arial',
+                  axisLabelPadding: 6
+            },
+            legend: {        
+                  labelBoxBorderColor: "#fff"
+            },
+      };
+    
+      for (var i=0; i<12; i++){
+            setX[i] = [{label:"position-x", data: globalDataX[i], color: "#00FF00" }];
+            setY[i] = [{label:"position-y", data: globalDataY[i], color: "#00FF00" }];
+            setSum[i] = [{label: "sum", data: globalDataSum[i], color: "#00FF00"}];
+      }
+    
+   // dataset = [{label:lab, data: globalData, color: "#00FF00" }];
+    
+      function update() {
+            GetData();
+            
+            for(var i=0; i<12; i++){
+                  $.plot($("#plotX"+i), setX[i], options)
+                  $.plot($("#plotY"+i), setY[i], options)
+                  $.plot($("#plotSum"+i), setSum[i], options)
+            }
+        //$.plot($(plot), dataset, options)
+        //setTimeout(update, updateInterval);
+      timeout_plot(update, updateInterval);
+      }
+      
+      update(); 
+}
+
---
+++ w3chaos/public_html/accumulatorOrbit/js/plot_tot_DD.js
@@ -0,0 +1,280 @@
+
+function builPlotTotDD(){
+    	
+    if ($('#plot_accumulator_x').length){
+
+	
+	/*$("#plot_accumulator_x").append('<fieldset class="plot col s12 m12 l12">' 
+				    + '<legend>Data X</legend><div class="col s11 m11 l11"><div id="chart_tot_x" style="width:100%; height:300px;"></div></div>'
+				    +'<div class="col s1 m1 l1" style="margin-top:2%;"><input type="checkbox" id="choices-load" class="filled-in"/><label for="choices-load" id="choices-load-labelX">dataset</label>'
+				    +'<input type="checkbox" id="choices-diff" class="filled-in"/><label for="choices-diff" id="choices-diff-labelX">diff</label></div>' 
+				    +'</fieldset>') */
+	
+	$("#plot_accumulator_x").append('<fieldset class="plot col s11 m12 l12">' 
+				    + '<legend>Data X</legend><div id="chart_tot_x" style="width:98%; height:300px;"></div>' 
+				    +'</fieldset>')
+
+	
+	$("#plot_accumulator_y").append('<fieldset class="plot col s11 m12 l12">' 
+				    + '<legend>Data Y</legend><div id="chart_tot_y" style="width:98%; height:300px;"></div>' 
+				    +'</fieldset>')
+   
+       
+    plotTotDD("#chart_tot_x","plotDataX");
+    plotTotDD("#chart_tot_y","plotDataY"); 
+
+ 
+    } else {
+		
+	$(".box-accumulator").append('<div id="plot_accumulator_x"></div>');
+		
+	$("#plot_accumulator_x").append('<fieldset class="plot col s11 m12 l12">' 
+				    + '<legend>Data X</legend><div id="chart_tot_x" style="width:98%; height:300px;"></div>' 
+				    +'</fieldset>')
+	
+	$(".box-accumulator").append('<div id="plot_accumulator_y"></div>');
+	
+	$("#plot_accumulator_y").append('<fieldset class="plot col s11 m12 l12">' 
+				    + '<legend>Data Y</legend><div id="chart_tot_y" style="width:98%; height:300px;"></div>' 
+				    +'</fieldset>')
+
+   
+    plotTotDD("#chart_tot_x","plotDataX");
+    plotTotDD("#chart_tot_y","plotDataY"); 
+
+    }
+} 
+
+
+
+function plotTotDD(chart,plotdataset) {
+        
+var globalDataXP = [];
+var globalDataYP = [];
+var globalDataXE = [];
+var globalDataYE = [];
+var datasetXP = [];
+var datasetYP = [];
+var datasetXE = [];
+var datasetYE = [];
+var datasetX = [];
+var datasetY = [];
+
+
+var updateInterval = frequency;
+
+var LoadDataXE = [];
+var LoadDataYE = [];
+var LoadDataXP = [];
+var LoadDataYP = [];
+
+var linac_st = 0;
+
+    function GetData() {
+   //     $.get("http://" + location.host + ":8081/CU?dev=ACCUMULATOR/BPM/BPMSYNC&cmd=status", function(datavalue, textStatus) {
+   //         var data_json = datavalue.replace(/\$numberLong/g, 'numberLong');
+    //        data_json = data_json.replace(/\//g, '');
+     //       var obj_json = JSON.parse(data_json);
+	    
+	    linac_st = 0;
+	    
+	    linac_st = obj_json.STATUSlinac_mode;
+	    // console.log("dafne get data " + linac_st );
+	    
+	    globalDataXP = [];
+	    globalDataYP = [];
+	    globalDataXE = [];
+	    globalDataYE = [];
+
+	    	    
+	    globalDataXP = [[2.8,obj_json.BPBA2002X_ACQ[0]],[5.2,obj_json.BPSA2001X_ACQ[0]],[6.6,obj_json.BPBA2001X_ACQ[0]],
+			  [9.7,obj_json.BPBA1002X_ACQ[0]],[11,obj_json.BPSA1001X_ACQ[0]],[13.5,obj_json.BPBA1001X_ACQ[0]],
+			  [17.7,obj_json.BPSA4001X_ACQ[0]],[19,obj_json.BPBA4002X_ACQ[0]],[22.9,obj_json.BPBA4001X_ACQ[0]],
+			  [26,obj_json.BPBA3002X_ACQ[0]],[29.8,obj_json.BPBA3001X_ACQ[0]],[31.1,obj_json.BPSA3001X_ACQ[0]]];
+	    
+	    globalDataYP = [[2.8,obj_json.BPBA2002Y_ACQ[0]],[5.2,obj_json.BPSA2001Y_ACQ[0]],[6.6,obj_json.BPBA2001Y_ACQ[0]],
+			  [9.7,obj_json.BPBA1002Y_ACQ[0]],[11,obj_json.BPSA1001Y_ACQ[0]],[13.5,obj_json.BPBA1001Y_ACQ[0]],
+			  [17.7,obj_json.BPSA4001Y_ACQ[0]],[19,obj_json.BPBA4002Y_ACQ[0]],[22.9,obj_json.BPBA4001Y_ACQ[0]],
+			  [26,obj_json.BPBA3002Y_ACQ[0]],[29.8,obj_json.BPBA3001Y_ACQ[0]],[31.1,obj_json.BPSA3001Y_ACQ[0]]];
+	    
+	    globalDataXE = [[2.8,obj_json.BPBA1001X_ACQ[0]],[5.2,obj_json.BPSA1001X_ACQ[0]],[6.6,obj_json.BPBA1002X_ACQ[0]],
+			  [9.7,obj_json.BPBA2001X_ACQ[0]],[11,obj_json.BPSA2001X_ACQ[0]],[13.5,obj_json.BPBA2002X_ACQ[0]],
+			  [17.7,obj_json.BPSA3001X_ACQ[0]],[19,obj_json.BPBA3001X_ACQ[0]],[22.9,obj_json.BPBA3002X_ACQ[0]],
+			  [26,obj_json.BPBA4001X_ACQ[0]],[29.8,obj_json.BPBA4002X_ACQ[0]],[31.1,obj_json.BPSA4001X_ACQ[0]]];
+	    
+	    globalDataYE = [[2.8,obj_json.BPBA1001Y_ACQ[0]],[5.2,obj_json.BPSA1001Y_ACQ[0]],[6.6,obj_json.BPBA1002Y_ACQ[0]],
+			  [9.7,obj_json.BPBA2001Y_ACQ[0]],[11,obj_json.BPSA2001Y_ACQ[0]],[13.5,obj_json.BPBA2002Y_ACQ[0]],
+			  [17.7,obj_json.BPSA3001Y_ACQ[0]],[19,obj_json.BPBA3001Y_ACQ[0]],[22.9,obj_json.BPBA3002Y_ACQ[0]],
+			  [26,obj_json.BPBA4001Y_ACQ[0]],[29.8,obj_json.BPBA4002Y_ACQ[0]],[31.1,obj_json.BPSA4001Y_ACQ[0]]];
+	    
+	    datasetXP = [{label:"currentDataX e+", data: globalDataXP, color: "#b30000" }];
+	    datasetYP = [{label:"currentDataY e+",data: globalDataYP, color: "#b30000"}];
+	    
+	    datasetXE = [{label:"currentDataX e-", data: globalDataXE, color: "#0062FF" }];
+	    datasetYE = [{label:"currentDataY e-",data: globalDataYE, color: "#0062FF"}];
+	    
+	    datasetX = [{label:"noise", data: globalDataXE, color: "#00FF00" }];
+	    datasetY = [{label:"noise",data: globalDataYE, color: "#00FF00"}];	
+
+
+//	});
+    }
+    
+    
+	
+    var optionsP = {
+	series: {
+	    lines: {
+		show: true,
+		lineWidth: 1.2
+	    }
+	},
+	xaxis: {
+	    tickSize: [1],
+	    ticks: [
+		    [2.8,"BA2002"],
+		    [5.2,"SA2001"],
+		    [6.6,"BA2001"],
+		    [9.7,"BA1002"],
+		    [11,"SA1001"],
+		    [13.5,"BA1001"],
+		    [17.7,"SA4001"],
+		    [19,"BA4002"],
+		    [22.9,"BA4001"],
+		    [26,"BA3002"],
+		    [29.8,"BA3001"],
+		    [31.1,"SA3001"],
+		    ],
+	    axisLabelUseCanvas: true,
+	    axisLabelFontSizePixels: 12,
+	    axisLabelFontFamily: 'Verdana, Arial',
+	    axisLabelPadding: 10
+	},
+	yaxis: {
+	    tickDecimals: [3],
+	    axisLabel: "[mm]",
+	    axisLabelUseCanvas: true,
+	    axisLabelFontSizePixels: 12,
+	    axisLabelFontFamily: 'Verdana, Arial',
+	    axisLabelPadding: 6
+	},
+	legend: {        
+	    labelBoxBorderColor: "#fff"
+	},
+    };
+    
+        var optionsE = {
+	series: {
+	    lines: {
+		show: true,
+		lineWidth: 1.2
+	    }
+	},
+	xaxis: {
+	    tickSize: [1],
+	    ticks: [
+		    [2.8,"BA1001"],
+		    [5.2,"SA1001"],
+		    [6.6,"BA1002"],
+		    [9.7,"BA2001"],
+		    [11,"SA2001"],
+		    [13.5,"BA2002"],
+		    [17.7,"SA3001"],
+		    [19,"BA3001"],
+		    [22.9,"BA3002"],
+		    [26,"BA4001"],
+		    [29.8,"BA4002"],
+		    [31.1,"SA4001"],
+		    ],
+	    axisLabelUseCanvas: true,
+	    axisLabelFontSizePixels: 12,
+	    axisLabelFontFamily: 'Verdana, Arial',
+	    axisLabelPadding: 10
+	},
+	yaxis: {
+	    tickDecimals: [3],
+	    axisLabel: "[mm]",
+	    axisLabelUseCanvas: true,
+	    axisLabelFontSizePixels: 12,
+	    axisLabelFontFamily: 'Verdana, Arial',
+	    axisLabelPadding: 6
+	},
+	legend: {        
+	    labelBoxBorderColor: "#000"
+	},
+    };
+    
+    function update() {
+        GetData();
+	
+	switch (linac_st) {
+	    case -1:
+		if (plotdataset == "plotDataX") {
+		    if (LoadDataXE.length) {
+			datasetXE.push({label: label_ele, data: LoadDataXE});
+			var diffXE= []; 
+			for(var i = 0; i< LoadDataXE.length; i++) {
+			    diffXE.push( [LoadDataXE[i][0], LoadDataXE[i][1] - globalDataXE[i][1]]);	
+			}
+			datasetXE.push({label: "difference", data: diffXE});
+		    }
+		    $.plot($(chart), datasetXE, optionsE);
+		}
+	
+		if (plotdataset == "plotDataY") {
+		    if (LoadDataYE.length) {
+			datasetYE.push({label:label_ele, data: LoadDataYE});
+			var diffYE= []; 
+			for(var i = 0; i< LoadDataYE.length; i++) {
+			    diffYE.push( [LoadDataYE[i][0], LoadDataYE[i][1] - globalDataYE[i][1]]);	
+			}
+			datasetYE.push({label: "difference", data: diffYE}); 
+		    }
+		    $.plot($(chart), datasetYE, optionsE)
+		}
+		break;
+	
+	    case 1:
+		if (plotdataset == "plotDataX") {
+		    if (LoadDataXP.length) {
+			datasetXP.push({label: label_pos, data: LoadDataXP});
+			var diffXP= []; 
+			for(var i = 0; i< LoadDataXP.length; i++) {
+			    diffXP.push( [LoadDataXP[i][0], LoadDataXP[i][1] - globalDataXP[i][1]]);	
+			}
+			datasetXP.push({label: "difference", data: diffXP}); 
+		    }
+		    $.plot($(chart), datasetXP, optionsP);
+		}
+	
+		if (plotdataset == "plotDataY") {
+		    if (LoadDataYP.length) {
+			datasetYP.push({label: label_pos, data: LoadDataYP});
+			var diffYP= []; 
+			for(var i = 0; i< LoadDataYP.length; i++) {
+			    diffYP.push( [LoadDataYP[i][0], LoadDataYP[i][1] - globalDataYP[i][1]]);	
+			}
+			datasetYP.push({label: "difference", data: diffYP}); 
+		    }
+		    $.plot($(chart), datasetYP, optionsP)
+		}
+		break;
+	    
+	    default:
+		if (plotdataset == "plotDataX") {	
+		    $.plot($(chart), datasetX, optionsE);
+		}
+		if (plotdataset == "plotDataY") {	
+		    $.plot($(chart), datasetY, optionsE)
+		}
+	
+	}
+	
+	timeout_plot(update, updateInterval);
+    } 
+    update(); 
+}
+
+
+
+
