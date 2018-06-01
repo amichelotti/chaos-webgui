322595fb35a608750b94b0b226ad9ba0a805b8ad
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2018-06-01 16:33:35 +0200

    moving WANProxy

---
+++ ChaosWANProxy/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/michelo/chaos_bundle/chaosframework/ChaosWANProxy
+# Build directory: /home/michelo/chaos_bundle/chaosframework/ChaosWANProxy
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
