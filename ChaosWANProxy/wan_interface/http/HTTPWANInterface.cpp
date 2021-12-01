e465603b1d49270a9bf1f0d07a33282c8e97157a
amichelo <andrea.michelotti@lnf.infn.it>
2021-12-01 16:36:52 +0100

    cleanup code toward boost removal

--- ChaosWANProxy/wan_interface/http/HTTPWANInterface.cpp
+++ ChaosWANProxy/wan_interface/http/HTTPWANInterface.cpp
@@ -129,7 +129,7 @@ void HTTPWANInterface::init(void *init_data) throw(CException) {
 		
 		//configure server
 		HTTWAN_INTERFACE_APP_ << " Thread " << idx << " allocated";
-        std::string str_port = boost::lexical_cast<std::string>(service_port);
+        std::string str_port = ChaosToString(service_port);
         mg_set_option(http_server, "listening_port", str_port.c_str());
 		mg_set_option(http_server, "enable_keep_alive", "yes");
 		mg_set_option(http_server, "enable_directory_listing", "false");
